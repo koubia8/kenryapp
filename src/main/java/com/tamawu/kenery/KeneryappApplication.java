13a49460aab378f8723231439dcda267de15d60d
koubia8 <koubia8@gmail.com>
2018-05-29 08:03:20 +0000

    gestion utilisateur

---
+++ src/main/java/com/tamawu/kenery/KeneryappApplication.java
@@ -0,0 +1,12 @@
+package com.tamawu.kenery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeneryappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeneryappApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tamawu/kenery/KeneryappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tamawu.kenery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KeneryappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
